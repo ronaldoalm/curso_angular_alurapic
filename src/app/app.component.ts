d18e4fcce8508be01dca32cfd23bad6a1c3b57dd
ronaldo almeida <ronaldoalm@unifor.br>
2021-05-04 18:43:44 -0300

    primeiro commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  description = 'Lion';
+  url= 'https://img.r7.com/images/moya-leao-branco-albino-atracao-glen-garriff-conservation-lion-sanctuary-23032021161516582?dimensions=771x420&no_crop=true';
+}
