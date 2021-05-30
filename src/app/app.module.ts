a76c1b4e62a2cef04576a46287d6482838e23a52
ronaldo almeida <ronaldoalm@unifor.br>
2021-05-30 10:37:46 -0300

    Lidando com rotas inexistentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
+import { ErrosModule } from './erros/erros.module';
 import { PhotosModule } from './photos/photos.module';
 
 
@@ -13,6 +14,7 @@ import { PhotosModule } from './photos/photos.module';
   imports: [
     BrowserModule,
     PhotosModule,
+    ErrosModule,
     AppRoutingModule
     
   ],
