96adad5ba88b7f1a9e3707ffec17104f6ceb1705
ronaldo almeida <ronaldoalm@unifor.br>
2021-05-30 09:49:17 -0300

    Single page e Rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
 import { PhotosModule } from './photos/photos.module';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { PhotosModule } from './photos/photos.module';
   ],
   imports: [
     BrowserModule,
-    PhotosModule
+    PhotosModule,
+    AppRoutingModule
     
   ],
   providers: [],
